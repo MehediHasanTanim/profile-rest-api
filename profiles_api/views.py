3ce680a5769f30f6d1242545fb666f8cca6ae5ac
MehediHasanTanim <tanim.cse.buet@gmail.com>
2024-01-18 15:34:51 +0600

    Added Profile Viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,16 @@
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
         """Returns a list of API View features"""
@@ -20,3 +27,52 @@ class HelloApiView(APIView):
                 'an_apiview' : an_apiview,
             }
         )
+    
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response(
+                {
+                    'message' : message
+                }
+            )
+        else:
+            return Response(
+                serializer.errors, 
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response(
+            {
+                'method' : 'PUT'
+            }
+        )
+    
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response(
+            {
+                'method' : 'PATCH'
+            }
+        )
+    
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response(
+            {
+                'method' : 'DELETE'
+            }
+        )
+    
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and update profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
